a4505c15886bbf3bd69a3264bee97d4680a70db8
Angelica Basista <abbasista1@up.edu.ph>
2021-04-27 23:19:50 +0800

    save events to database

--- lib/services/db.dart
+++ lib/services/db.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 class DBService {
   final CollectionReference taskCollection = FirebaseFirestore.instance.collection('tasks');
+  final CollectionReference eventCollection = FirebaseFirestore.instance.collection('events');
   var user = FirebaseAuth.instance.currentUser;
 
   Future getTasks() async {
@@ -24,6 +26,34 @@ class DBService {
       'description': description,
       'startDate': startDate,
       'dueDate': dueDate,
+      'categories': categories,
+      'reminder': reminder,
+    });
+  }
+
+  Future getEvents() async {
+    var events = await eventCollection.where('email', isEqualTo: user.email).get();
+    var parsedEvents = events.docs.map( (event) {
+      var val = event.data();
+      val['id'] = event.id;
+      return val;
+    }).toList();
+
+    return parsedEvents; 
+  }
+
+  // To do: Add category and date fields
+  Future createEvent(String title, String notes, TimeOfDay startTime, TimeOfDay endTime, DateTime date, List<String> categories, var reminder) async {
+   
+    DateTime startTimestamp = new DateTime(date.year, date.month, date.day, startTime.hour, startTime.minute);
+    DateTime endTimestamp = new DateTime(date.year, date.month, date.day, endTime.hour, endTime.minute);
+
+    return eventCollection.add({
+      'email': user.email,
+      'title': title,
+      'notes': notes,
+      'startTime': startTimestamp, // Timestamp data type
+      'endTime': endTimestamp,     // Timestamp data type
       'categories': categories,
       'reminder': reminder,
     });
