70511fc557e9e7ff5f9131bdc17aba936b789ccd
iMaro40 <maro.santiago@yahoo.com>
2021-05-22 15:31:20 +0800

    display user events in calendar

--- lib/utils/calendar.dart
+++ lib/utils/calendar.dart
@@ -25,13 +25,7 @@ final kEvents = LinkedHashMap<DateTime, List<Event>>(
 final _kEventSource = Map.fromIterable(List.generate(50, (index) => index),
     key: (item) => DateTime.utc(2020, 10, item * 5),
     value: (item) => List.generate(
-        item % 4 + 1, (index) => Event('Event $item | ${index + 1}')))
-  ..addAll({
-    DateTime.now(): [
-      Event('Today\'s Event 1'),
-      Event('Today\'s Event 2'),
-    ],
-  });
+        item % 4 + 1, (index) => Event('Event $item | ${index + 1}')));
 
 int getHashCode(DateTime key) {
   return key.day * 1000000 + key.month * 10000 + key.year;
