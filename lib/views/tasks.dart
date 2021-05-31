396900bbd8529451b2e98134dd782618e4cca7ee
iMaro40 <maro.santiago@yahoo.com>
2021-06-01 01:09:11 +0800

    working categories filter

--- lib/views/tasks.dart
+++ lib/views/tasks.dart
@@ -13,108 +13,124 @@ class Tasks extends StatefulWidget {
 
 
 class _Tasks extends State<Tasks> {
-  @override
-  Widget build(BuildContext context) {
-    final DBService db = DBService();
-    return FutureBuilder(
-      future: db.getTasks(),
-      builder: (context, snapshot) {
-        if(snapshot.hasData) {
-          dynamic tasks = snapshot.data;
-          dynamic toDisplay = tasks;
-          
-          Set categoriesSet = Set();
+  String selectedCategory = 'All';
+  
+  Set categoriesSet = Set();
+  
+
+  final DBService db = DBService();
+  dynamic tasks = [];
+  dynamic toDisplay = [];
+
+  void initState(){
+      super.initState();
+      
+      //add 'All' to display all tasks
+      categoriesSet.add('All');
+
+      db.getTasks().then((tasksData) {
+        setState(() {
+          tasks = tasksData;
+          toDisplay = tasks;
           for(dynamic task in tasks) {
             for(dynamic category in task['categories']) {
               categoriesSet.add(category);
             }
           }
           
-          String selectedCategory = categoriesSet.first;
+          selectedCategory = categoriesSet.first.toString();
+        });
+      });
+  }
 
-          return Scaffold(
-            resizeToAvoidBottomInset: true,
-            body: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 80.0),
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child:
-                        Column(
-                          children: [
-                            Text('MY TASKS', style: header_text), 
-                            DropdownButton(
-                              value: selectedCategory,
-                              items: categoriesSet.toList()
-                                  .map<DropdownMenuItem<String>>((value) {
-                                return DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(value),
-                                );
-                              }).toList(),
-                              onChanged: (String? newValue) {
-                                setState(() {
-                                  selectedCategory = newValue!;
-                                });
-                              },
-                            )
-                          ],
-                        )
-                    ), 
-                    SizedBox(height: 50.0),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Row( //Task category initials
-                        children: [
-                          TaskCategory(category_initial: 'A'), //fetch category initial
-                          TaskCategory(category_initial: 'W'),
-                        ],
-                      ),
-                    ), 
-                    Divider(
-                      height: 20,
-                      thickness: 2,
-                      indent: 20,
-                      endIndent: 20,
-                    ),
-                    ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: toDisplay?.length,
-                      itemBuilder: (context, index) {
-                        return Column(//DISPLAY TASKS HERE
-                          children: [
-                            Text(toDisplay[index]['title']),
-                            SizedBox(height: 5.0)
-                          ],
-                        );
-                      }
-                    ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 80.0),
+              Align(
+                alignment: Alignment.topLeft,
+                child:
+                  Column(
+                    children: [
+                      Text('MY TASKS', style: header_text), 
+                      DropdownButton(
+                        value: selectedCategory,
+                        items: categoriesSet.toList()
+                            .map<DropdownMenuItem<String>>((value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            selectedCategory = newValue!;
+                            if(selectedCategory == 'All') {
+                              toDisplay = tasks;
+                            }
+                            else {
+                              toDisplay = [];
+                              for(dynamic task in tasks) {
+                                if(task['categories'].contains(selectedCategory)) {
+                                  toDisplay.add(task);
+                                }
+                              }
+                            }
+                          });
+                        },
+                      )
+                    ],
+                  )
+              ), 
+              SizedBox(height: 50.0),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row( //Task category initials
+                  children: [
+                    TaskCategory(category_initial: 'A'), //fetch category initial
+                    TaskCategory(category_initial: 'W'),
                   ],
                 ),
+              ), 
+              Divider(
+                height: 20,
+                thickness: 2,
+                indent: 20,
+                endIndent: 20,
               ),
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AddTask()),
-                );
-              },
-              child: const Icon(Icons.add),
-              backgroundColor: dark_blue,
-            ),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: toDisplay?.length,
+                itemBuilder: (context, index) {
+                  return Column(//DISPLAY TASKS HERE
+                    children: [
+                      Text(toDisplay[index]['title']),
+                      SizedBox(height: 5.0)
+                    ],
+                  );
+                }
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddTask()),
           );
-        }
-        else {
-          return Container();
-        }
-      }
-        
-    );
-    
+        },
+        child: const Icon(Icons.add),
+        backgroundColor: dark_blue,
+      ),
+    );    
   }
 }
 
